164fa519e6b61f08d7e2cd2efd9e91481dbc01d9
Leo Gao <leogao31@gmail.com>
2021-02-23 19:19:51 -0700

    Update to download image urls for cc

--- download_commoncrawl.py
+++ download_commoncrawl.py
@@ -20,7 +20,8 @@ import json
 import abc
 
 
-mode = 'justext'
+#mode = 'justext'
+mode = 'cc_imgs'
 
 
 blocks_to_download = sys.argv[1].split(',')
@@ -57,9 +58,15 @@ def urls_of_block(block):
         yield from map(lambda x: "https://commoncrawl.s3.amazonaws.com/" + x, fh)
 
 
-def warcurl_to_contents(warc_url):
+def wat_urls_of_block(block):
+    with open('warc_blocks/urls_' + block.rjust(4, '0')) as fh:
+        yield from map(lambda x: "https://commoncrawl.s3.amazonaws.com/" + x.replace('warc.gz', 'warc.wat.gz').replace('/warc/', '/wat/'), fh)
+
+
+def warcurl_to_contents(warc_url, wat=False):
     try:
         response = requests.get(warc_url.strip(), stream=True)
+
         for record in ArchiveIterator(response.raw, arc2warc=True):
             if record.rec_type == 'response':
                 content = record.content_stream().read()
@@ -70,22 +77,36 @@ def warcurl_to_contents(warc_url):
                 }
 
                 yield content, meta
+            if wat and record.rec_type == 'metadata':
+                content = record.content_stream().read()
+                meta = {
+                    'warc': warc_url.strip(),
+                    'target': record.rec_headers.get_header('WARC-Target-URI'),
+                }
+
+                yield content, meta
     except warcio.exceptions.ArchiveLoadFailed:
         print('WARNING: WARC load failed!')
         traceback.print_exc()
 
 
-def warcurls_to_contents(warc_urls):
+def warcurls_to_contents(warc_urls, wat=False):
     for url in tqdm(list(warc_urls)):
-        yield from warcurl_to_contents(url)
+        yield from warcurl_to_contents(url, wat)
 
 
 import pycld2 as cld2
 import justext
 import lxml
 
+try:
+    from urlparse import urljoin  # Python2
+except ImportError:
+    from urllib.parse import urljoin  # Python3
 
-langdet = fasttext.load_model("lid.176.bin") 
+from best_download import download_file
+import fasttext
+download_file('https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin', 'lid.176.bin', '7e69ec5451bc261cc7844e49e4792a85d7f09c06789ec800fc4a44aec362764e')
 
 
 # todo: make HtmlExtractor class to seperate justext and trafilatura logic
@@ -144,6 +165,23 @@ def html_to_text(args):
                 **meta
             }
             return result, meta
+        elif mode == 'cc_imgs':
+            try:
+                obs = json.loads(html)['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']
+                
+                cc_urls = [ob for ob in obs if ob['url'].startswith('http://creativecommons.org/licenses/')]
+                keep = len(cc_urls) > 0
+                if not keep: return None
+
+                obs = [{'url': urljoin(meta['target'], ob['url']), 'alt': ob['alt']} for ob in obs if ob['path'] == 'IMG@/src' and 'alt' in ob and ob['alt']]
+
+                meta = {
+                    'cc_urls': cc_urls,
+                    **meta
+                }
+                return obs, meta
+            except KeyError:
+                return
         else:
             raise AssertionError('unknown mode!')
     except lxml.etree.ParserError:
@@ -152,11 +190,11 @@ def html_to_text(args):
         traceback.print_exc()
 
 
-def get_cc_text(warc_urls, html_to_text):
+def get_cc_text(warc_urls, html_to_text, wat=False):
     pool = mp.Pool(num_threads)
 
     yield from filter(lambda x:x and x[0],
-                      pool.imap(html_to_text, warcurls_to_contents(warc_urls)))
+                      pool.imap(html_to_text, warcurls_to_contents(warc_urls, wat)))
 
 
 class Hook(abc.ABC):
@@ -179,6 +217,7 @@ class ArchiveHook(Hook):
         lang = meta['primary_language']
         if lang not in self.ars:
             self.ars[lang] = lmd.Archive(f'output/{lang}', compression_level=7)
+        self.ars[lang].add_data(doc, meta)
         self.ct_by_lang[lang] += 1
         self.total_docs += 1
 
@@ -192,22 +231,49 @@ class ArchiveHook(Hook):
         self.ars = {}
         self.total_docs = 0
         self.ct_by_lang = collections.defaultdict(int)
+
+
+class DebugHook(Hook):
+    def __init__(self):
+        self.ars = {}
+        self.total_docs = 0
+        self.ct_by_lang = collections.defaultdict(int)
+
+    def write_doc(self, doc, meta):
+        print(doc, meta)
+
+    def commit_block(self, block):
+        pass
+
+
+class SimpleArchiveHook(Hook):
+    def __init__(self):
+        self.ar = lmd.Archive(f'output', compression_level=7)
+        self.ct_by_lang = collections.defaultdict(int)
+
+    def write_doc(self, doc, meta):
+        self.ar.add_data(doc, meta)
+
+    def commit_block(self, block):
+        self.ar.commit(archive_name=block)
             
 
 
-def download(blocks, html_to_text, keep_doc, hooks):
+def download(blocks, html_to_text, keep_doc, hooks, wat=False):
     for block in blocks:
         print('Downloading block', block)
-        warcurls = urls_of_block(block)
+        warcurls = wat_urls_of_block(block) if wat else urls_of_block(block)
 
-        for text, meta in get_cc_text(warcurls, html_to_text):
+        for text, meta in get_cc_text(warcurls, html_to_text, wat):
             if keep_doc(text):
                 for hook in hooks: hook.write_doc(text, meta)
         
         for hook in hooks: hook.commit_block(block)
 
+
 def keep_doc(doc):
     return True
 
 if __name__ == '__main__':
-    download(blocks_to_download, html_to_text, keep_doc, [ArchiveHook()])
+    #download(blocks_to_download, html_to_text, keep_doc, [ArchiveHook()])
+    download(blocks_to_download, html_to_text, keep_doc, [SimpleArchiveHook()], wat=True)
